71486df1c41a8513fb0a499b2f614585b09ace4a
Gabor <benkogabor@msn.com>
2018-10-14 19:12:19 +0200

    canny edge

--- detection.py
+++ detection.py
@@ -7,10 +7,14 @@ def grayscale(img):
 def gaussian_blur(img, kernel_size):
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
+def canny_edge(img, low_tershold, high_tershold):
+    return cv2.Canny(img, low_tershold, high_tershold)
+
 img = cv2.imread("/home/bennyg/Images/lane.jpg")
 gray = grayscale(img)
 blur = gaussian_blur(gray, 7)
+edge = canny_edge(img, 50, 125)
 
-cv2.imshow("image", blur)
+cv2.imshow("image", edge)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
