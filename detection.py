6f344e5d72c9622d587e54dcda6c8e1dab4e7f03
Gabor <benkogabor@msn.com>
2018-10-14 20:45:05 +0200

    lane line detection with hough transform

--- detection.py
+++ detection.py
@@ -22,6 +22,30 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
+def hough_lines(img, rho, theta, treshold, min_line_len, max_line_gap):
+    lines = cv2.HoughLinesP(img, rho, theta, treshold, np.array([]), minLineLength = min_line_len, maxLineGap = max_line_gap)
+    line_img = np.zeros(img.shape, dtype = np.uint8)
+    draw_lines(line_img, line_img)
+    return line_img
+
+def separate_lines(lines):
+    right = []
+    left = []
+    for x1,y1,x2,y2 in lines[:, 0]:
+        m = (float(y2) - y1) / (x2 - x1)
+        if m >= 0: 
+            right.append([x1,y1,x2,y2,m])
+        else:
+            left.append([x1,y1,x2,y2,m])
+    
+    return right, left
+
+def draw_lines(img, lines, color=(255, 0, 0), thickness=7):
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+
 def lane_detection_pipeline(img):
     ### Params for region of interest
     bot_left = [80, 540]
@@ -33,7 +57,11 @@ def lane_detection_pipeline(img):
     blur = gaussian_blur(gray, 7)
     edge = canny_edge(img, 50, 125)
     masked = region_of_interest(edge, vertices)
-    return masked
+    lines = cv2.HoughLinesP(masked, 0.8, np.pi/180, 25, np.array([]), minLineLength=50, maxLineGap=200)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    return img
 
 img = cv2.imread("/home/bennyg/Images/lane.jpg")
 final_img = lane_detection_pipeline(img)
