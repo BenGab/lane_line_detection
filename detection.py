6102687ba1cd3f02728fcb8b7a7b50946f385d48
Gabor <benkogabor@msn.com>
2018-10-14 18:59:48 +0200

    grayscale function

--- detection.py
+++ detection.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("/home/bennyg/Images/lane.jpg")
+def grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("image", img)
+img = cv2.imread("/home/bennyg/Images/lane.jpg")
+gray = grayscale(img)
+
+cv2.imshow("image", gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
